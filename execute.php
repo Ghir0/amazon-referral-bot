bef215a6c71c2d2ecf4b9f09fad4f49c07b1fa7f
fabio841 <59645608+fabio841@users.noreply.github.com>
2020-01-21 07:00:43 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,100 +19,75 @@ $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 // pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
 $text = trim($text);
-//$text = strtolower($text);
-$array1 = array();
-		
+
+$proprietario = "";
 // gestisco la richiesta
 $response = "";
 if(isset($message['text']))
+
 {
-  $arr = explode("http", $text, 2);
-  $testoLink = $arr[0];
-  
-  $dominioAmazon = get_string_between($text, "://www.", ".it");
-  $dominioGearbest = get_string_between($text, "://www.", ".com");
-  $dominioAmazonShorten = get_string_between($text, "://", ".to");
-	
   //NUOVO PARSER:
-  //$text_url_array = parse_text($text);
-  
-  $text_url_array = getUrls($text);
-	
-  //$array1 = explode('.', $text_url_array[1]);
-  //$dominio = $array1[1];
-  //test url $string_test = var_export($array1, true);
-	
+  $text_url_array = parse_text($text);
+
   if(strpos($text, "/start") === 0 )
   {
-	$response = "Ciao $firstname! \nMandami un link Amazon o condividilo direttamente con me da altre app! \nTi rispondero' con il link affiliato del mio padrone! Grazie mille!\n\nCreated by http://www.webemento.com";
+	$response = "Ciao $firstname! \nMandami un link Amazon preceduto da /link! \nTi rispondero' con un link affiliate!";
   }
-  elseif($dominioAmazon == "amazon")
+  elseif(strpos($text, "/link") === 0 && strlen($text)>6 )
   {	  
 	//new parser:
-	$url_to_parse = $text_url_array[0];
+	$url_to_parse = $text_url_array[1];
 	$url_affiliate = set_referral_URL($url_to_parse);
 	$faccinasym = json_decode('"\uD83D\uDE0A"');
 	$linksym =  json_decode('"\uD83D\uDD17"');
 	$pollicesym =  json_decode('"\uD83D\uDC4D"');
 	$worldsym = json_decode('"\uD83C\uDF0F"');
-	$obj_desc = $testoLink;
+	$obj_desc = $text_url_array[0];
 	$short = make_bitly_url($url_affiliate,'ghir0','json');
-	$response = "$obj_desc\n$worldsym $short";
+	$response = "Ecco il link di $proprietario: $obj_desc\n$worldsym  $short";
 	
   }
-	elseif($dominioAmazonShorten=="amzn")
-  {     
-       //new parser:
-       $url_to_parse = $text_url_array[0];
-       $url_affiliate = set_referral_URL_amazon_shorten($url_to_parse);
-       $faccinasym = json_decode('"\uD83D\uDE0A"');
-       $linksym =  json_decode('"\uD83D\uDD17"');
-       $pollicesym =  json_decode('"\uD83D\uDC4D"');
-       $worldsym = json_decode('"\uD83C\uDF0F"');
-       $obj_desc = $testoLink;
-       $short = make_bitly_url($url_affiliate,'ghir0','json');
-       $response = "$obj_desc\n$worldsym $short";
-   }
-   elseif($dominioGearbest == "gearbest")
-   {
-	$url_to_parse = $text_url_array[0];
-	$url_affiliate = set_referral_URL_GB($url_to_parse);
+  elseif(strpos($text, "/mike") === 0 && strlen($text)>6 )
+  {	  
+	//new parser:
+	$url_to_parse = $text_url_array[1];
+	$url_affiliate = set_mike_referral_URL($url_to_parse);
 	$faccinasym = json_decode('"\uD83D\uDE0A"');
 	$linksym =  json_decode('"\uD83D\uDD17"');
 	$pollicesym =  json_decode('"\uD83D\uDC4D"');
 	$worldsym = json_decode('"\uD83C\uDF0F"');
-	$obj_desc = $testoLink;
+	$obj_desc = $text_url_array[0];
 	$short = make_bitly_url($url_affiliate,'ghir0','json');
-	$response = "$obj_desc\n$worldsym  $short";
-   }
+	$response = "Ecco il link di Mike: $obj_desc\n$worldsym  $short";
+	
+  }
+  elseif(strpos($text, "/dc") === 0 && strlen($text)>6 )
+  {	  
+	//new parser:
+	$url_to_parse = $text_url_array[1];
+	$url_affiliate = set_dc_referral_URL($url_to_parse);
+	$faccinasym = json_decode('"\uD83D\uDE0A"');
+	$linksym =  json_decode('"\uD83D\uDD17"');
+	$pollicesym =  json_decode('"\uD83D\uDC4D"');
+	$worldsym = json_decode('"\uD83C\uDF0F"');
+	$obj_desc = $text_url_array[0];
+	$short = make_bitly_url($url_affiliate,'ghir0','json');
+	$response = "Ecco il link di Diego: $obj_desc\n$worldsym  $short";
+	
+  }
    elseif(strpos($text, "/link") === 0 && strlen($text)<6 )
   {
-	   //$response = "Incolla l'URL Amazon da convertire dopo il comando /link";
+	   $response = "Incolla l'URL da convertire dopo il comando /link";
    }
-  else {
-	  //$response = "$string_test";
-  }
-}
-/*
-*
-* prende un link amazon, estrapola l'ASIN e ricrea un link allo stesso prodotto con il referral
-*/
-function set_referral_URL_amazon_shorten($url){
-       $referral = "diev-21";
-       $url_edited = "";
-       $parsed_url_array = parse_url($url);   
-       $seller = strstr($parsed_url_array['query'], 'm=');     
-       $parsed = extract_unit($fullstring, 'm=', '&');
-       $seller = "&".$seller;
-       $url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral.$seller;
-       return $url_edited;
 }
 /*
 *
 * prende un link amazon, estrapola l'ASIN e ricrea un link allo stesso prodotto con il referral 
 */
 function set_referral_URL($url){
-	$referral = "diev-21";
+	$referral = array("miketama-21","s1m0nex27-21","antonio99-21","antcaiazza-21");
+	$random = mt_rand(0,3);
+	$GLOBALS['proprietario'] = $referral[$random];
 	$url_edited = "";
 	$parsed_url_array = parse_url($url);
 	
@@ -120,36 +95,44 @@ function set_referral_URL($url){
 	
 	$parsed = extract_unit($fullstring, 'm=', '&');
 	$seller = "&".$seller;
-	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral.$seller;
+	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral[$random].$seller;
 	return $url_edited;
 }
-/*
-*
-* crea il link con referral di gearbest 
-*/
-function set_referral_URL_GB($url){
-	$referral = "10851947";
+function set_mike_referral_URL($url){
+	$referral_mike = "miketama-21";
+	
 	$url_edited = "";
 	$parsed_url_array = parse_url($url);
 	
 	$seller = strstr($parsed_url_array['query'], 'm=');
 	
 	$parsed = extract_unit($fullstring, 'm=', '&');
-	//$seller = "&".$seller;
-	$url_edited = "http://www.gearbest.com".$parsed_url_array['path']."?lkid=".$referral.$seller;
+	$seller = "&".$seller;
+	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral_mike.$seller;
 	return $url_edited;
 }
-function getUrls($string) {
- $regex = '/https?\:\/\/[^\" ]+/i';
- preg_match_all($regex, $string, $matches);
- return ($matches[0]);
+function set_dc_referral_URL($url){
+	$referral_dc = "amazonscont0e-21";
+	
+	$url_edited = "";
+	$parsed_url_array = parse_url($url);
+	
+	$seller = strstr($parsed_url_array['query'], 'm=');
+	
+	$parsed = extract_unit($fullstring, 'm=', '&');
+	$seller = "&".$seller;
+	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral_dc.$seller."keywords=I%20Consigli%20di%20Diegocampy.it";
+	return $url_edited;
 }
+
 //nuovo parser
 function parse_text($string){
-	$string2 = str_replace("/link", "", $string);
-	preg_match_all('%^((https?://)|(www\.))([a-z0-9-].?)+(:[0-9]+)?(/.*)?$%i', $string2, $match);
+	$string1 = str_replace("/dc", "", $string);
+	$string2 = str_replace("/link", "", $string1);
+	$string3 = str_replace("/mike", "", $string2);
+	preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $string3, $match);
 	$text_parsed_URL = $match[0][0];
-	$arr = explode("http", $string2);
+	$arr = explode("http", $string3);
 	$text_parsed_TEXT = $arr[0];
 	$text_parsed = array($text_parsed_TEXT, $text_parsed_URL);
 	return $text_parsed;
@@ -163,28 +146,6 @@ function extract_unit($string, $start, $end){
 	$str_three = substr($str_two, 0, $second_pos);
 	$unit = trim($str_three); // remove whitespaces
 	return $unit;
-}
-function strbefore($string, $substring) {
-  $pos = strpos($string, $substring);
-  if ($pos === false)
-   return $string;
-  else  
-   return(substr($string, 0, $pos));
-}
-function strafter($string, $substring) {
-  $pos = strpos($string, $substring);
-  if ($pos === false)
-   return $string;
-  else  
-   return(substr($string, $pos+strlen($substring)));
-}
-function get_string_between($string, $start, $end){
-    $string = ' ' . $string;
-    $ini = strpos($string, $start);
-    if ($ini == 0) return '';
-    $ini += strlen($start);
-    $len = strpos($string, $end, $ini) - $ini;
-    return substr($string, $ini, $len);
 }
 function make_bitly_url($url,$login,$format = 'xml',$version = '2.0.1')
 {
@@ -207,6 +168,22 @@ function make_bitly_url($url,$login,$format = 'xml',$version = '2.0.1')
 		return 'http://bit.ly/'.$xml->results->nodeKeyVal->hash;
 	}
 }
+
+/*function get_string_between($string, $start, $end){
+	$string = ' ' . $string;
+	$ini = strpos($string, $start);
+	if ($ini == 0) return '';
+	$ini += strlen($start);
+	$len = strpos($string, $end, $ini) - $ini;
+	return substr($string, $ini, $len);
+}
+
+function clean_for_URL($string){
+	$cleaned_string = explode(' ',strstr($string,'https://'))[0];
+	if(strcmp($cleaned_string,"false") == "0"){ $cleaned_string = explode(' ',strstr($string,'http://'))[0]; }
+	return $cleaned_string;
+}
+*/
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
